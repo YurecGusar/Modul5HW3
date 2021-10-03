daa9420ed205bfad9608dd317e9a46557490c9a1
Yurii <leoonovn@gmail.com>
2021-10-03 15:30:12 +0300

    master

---
+++ index.jsx
@@ -0,0 +1,42 @@
+const constNum = 3;
+const constDegree = 3;
+const constSquareSize = "200px";
+const constColor = "red";
+
+class PowComponent extends React.Component {
+    render() {
+
+        let _num = this.props.num;
+        let _degree = this.props.degree;
+
+        return  <div>
+                    {_num}<sup>{_degree}</sup> = {Math.pow(_num, _degree)}
+                </div>;
+    }
+}
+
+class SquareComponent extends React.Component
+{
+    render(){
+
+        let _squareSize = this.props.squareSize;
+        let _color = this.props.color;
+
+        return <div style = {{width : _squareSize, height : _squareSize, backgroundColor : _color}}></div>
+    }
+}
+
+class Container extends React.Component
+{
+    render(){
+        return  <div>
+                    <PowComponent num={constNum} degree={constDegree} />
+                    <SquareComponent squareSize={constSquareSize} color= {constColor} />
+                </div>
+    }
+}
+
+ReactDOM.render(
+    <Container/>,
+    document.getElementById("app")
+)
